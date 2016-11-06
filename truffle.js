4cd35949e571eb2df08f67635fbf05c3ec20fe9d
Zac Mitton <zacmitton22@gmail.com>
2016-11-06 13:57:52 -0600

    deployable to testrpc

--- truffle.js
+++ truffle.js
@@ -12,5 +12,10 @@ module.exports = {
   rpc: {
     host: "localhost",
     port: 8545
-  }
+  },
+  networks: {
+    "live": { network_id: 1 },
+    "morden": { network_id: 2 },
+    "consensys": { network_id: 161 },
+  },
 };
