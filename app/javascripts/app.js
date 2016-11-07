3fa03a7c4f161257585c5ed76a697ac86d3e9fb9
Zac Mitton <zacmitton22@gmail.com>
2016-11-06 18:28:43 -0600

    frontend interface for sending first TX working

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,40 +1,63 @@
+var BigNumber = require("bignumber.js");
 var accounts;
 var account;
 var balance;
+
+function createGame() {
+  var flipper = Flipper.deployed();
+
+  var amount = new BigNumber(document.getElementById("amount").value * 1e18).toString();
+
+  setStatus("Initiating transaction... (please wait)");
+
+  flipper.createGame({from: account, value: amount, gas: 500000}).then(function() {
+    setStatus("Transaction complete!");
+    refreshThings();
+  }).catch(function(e) {
+    console.log(e);
+    setStatus("Error sending coin; see log.");
+  });
+};
+
 
 function setStatus(message) {
   var status = document.getElementById("status");
   status.innerHTML = message;
 };
 
-function refreshBalance() {
-  var meta = MetaCoin.deployed();
+function refreshThings() {
+  var flipper = Flipper.deployed();
 
-  meta.getBalance.call(account, {from: account}).then(function(value) {
-    var balance_element = document.getElementById("balance");
-    balance_element.innerHTML = value.valueOf();
+  flipper.game.call({from: account}).then(function(stateNum) {
+    gameStates = ["open", "offerMade", "gameOn"];
+    var gs = document.getElementById("gameState");
+    gs.innerHTML = gameStates[stateNum.valueOf()];
+    return flipper.buyIn.call({from: account})
+    .then(function(buyIn) {
+      console.log(buyIn.toString());
+    })
   }).catch(function(e) {
     console.log(e);
     setStatus("Error getting balance; see log.");
   });
 };
 
-function sendCoin() {
-  var meta = MetaCoin.deployed();
+// function sendCoin() {
+//   var meta = MetaCoin.deployed();
 
-  var amount = parseInt(document.getElementById("amount").value);
-  var receiver = document.getElementById("receiver").value;
+//   var amount = parseInt(document.getElementById("amount").value);
+//   var receiver = document.getElementById("receiver").value;
 
-  setStatus("Initiating transaction... (please wait)");
+//   setStatus("Initiating transaction... (please wait)");
 
-  meta.sendCoin(receiver, amount, {from: account}).then(function() {
-    setStatus("Transaction complete!");
-    refreshBalance();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error sending coin; see log.");
-  });
-};
+//   meta.sendCoin(receiver, amount, {from: account}).then(function() {
+//     setStatus("Transaction complete!");
+//     refreshBalance();
+//   }).catch(function(e) {
+//     console.log(e);
+//     setStatus("Error sending coin; see log.");
+//   });
+// };
 
 window.onload = function() {
   web3.eth.getAccounts(function(err, accs) {
@@ -51,6 +74,6 @@ window.onload = function() {
     accounts = accs;
     account = accounts[0];
 
-    refreshBalance();
+    refreshThings();
   });
 }
